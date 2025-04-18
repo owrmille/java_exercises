7bcd6d5f3a9b3b8e1dd2d3ed5b0b809cb17b6a09
owrmille <iasemin.topchu@gmail.com>
2025-04-18 22:21:04 +0200

    Add findMinMax exercise

--- stepik_java/StreamApiExercises.java
+++ stepik_java/StreamApiExercises.java
@@ -1,5 +1,11 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamApiExercises {
     public static IntUnaryOperator getMiddle() {
@@ -11,7 +17,59 @@ public class StreamApiExercises {
         return randomNumbers;
     }
 
+    public static <T> void findMinMax(
+        Stream<? extends T> stream,
+        Comparator<? super T> order,
+        BiConsumer<? super T, ? super T> minMaxConsumer) {
+        
+        List<T> sortedValues = stream.collect(Collectors.toList());
+        if (sortedValues.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            sortedValues.sort(order);
+            T min = sortedValues.get(0);
+            T max =  sortedValues.get(sortedValues.size() - 1);
+            minMaxConsumer.accept(min, max);
+        }
+    }
+
+    public static <T> void alternativeFindMinMax(
+        Stream<? extends T> stream,
+        Comparator<? super T> order,
+        BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        MinMaxFinder<T> minMaxFinder = new MinMaxFinder<>(order);
+        stream.forEach(minMaxFinder);
+        minMaxConsumer.accept(minMaxFinder.min, minMaxFinder.max);
+    }
+
+    private static class MinMaxFinder<T> implements Consumer<T> {
+
+        private final Comparator<? super T> order;
+        T min;
+        T max;
+
+        private MinMaxFinder(Comparator<? super T> order) {
+            this.order = order;
+        }
+
+        @Override
+        public void accept(T t) {
+            if (min == null || order.compare(t, min) < 0) {
+                min = t;
+            }
+            if (max == null || order.compare(max, t) < 0) {
+                max = t;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         pseudoRandomStream(13).limit(12).forEach(System.out::println);
+
+        IntStream stream = pseudoRandomStream(13);
+        Comparator<Integer> order = Comparator.naturalOrder();
+        BiConsumer<Integer, Integer> minMaxConsumer = (min, max) -> {System.out.println("Min: " + min + ", Max: " + max);};
+        alternativeFindMinMax(stream.limit(12).boxed(), order, minMaxConsumer);
     }
 }
