59eb2d81e1ed950ee87b2d353cdfca1a3b86bce4
owrmille <iasemin.topchu@gmail.com>
2025-04-18 10:27:42 +0200

    Update CollectionsExercises

--- stepik_java/CollectionsExercises.java
+++ stepik_java/CollectionsExercises.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CollectionsExercises {
     public static <T> Set<T> findSymmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
@@ -25,7 +30,20 @@ public class CollectionsExercises {
         return symmetricDifference;
     }
 
-    public static void main(String[] args) {
+    public static List<Integer> removeNumbersOnEvenPositionsAndReverse() throws Exception {
+        List<Integer> numbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; scanner.hasNext(); i++) {
+            int value = scanner.nextInt();
+            if (i % 2 != 0) {
+                numbers.add(value);
+            }
+        }
+        Collections.reverse(numbers);
+        return numbers;
+    }
+
+    public static void main(String[] args) throws Exception {
         // String
         Set<String> set1 = new HashSet<>(Arrays.asList("sun", "fox", "orange"));
         Set<String> set2 = new HashSet<>(Arrays.asList("orange", "sun", "ball"));
@@ -35,5 +53,11 @@ public class CollectionsExercises {
         Set<Integer> set3 = new HashSet<>(Arrays.asList(42, 1, 108));
         Set<Integer> set4 = new HashSet<>(Arrays.asList(0, 1));
         System.out.println(alternativeFindSymmetricDifference(set3, set4));
+
+        List<Integer> numbers = removeNumbersOnEvenPositionsAndReverse();
+        String modifiedNumbers = numbers.stream()
+                                        .map(String::valueOf)
+                                        .collect(Collectors.joining(" "));
+        System.out.println(modifiedNumbers);
     }
 }
