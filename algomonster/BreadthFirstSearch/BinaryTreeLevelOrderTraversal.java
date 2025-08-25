e06c53c6d2c17626f20ebef29436aaed1f9b5000
owrmille <iasemin.topchu@gmail.com>
2025-08-25 18:42:45 +0200

    Add BinaryTreeLevelOrderTraversal

---
+++ algomonster/BreadthFirstSearch/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,68 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class BinaryTreeLevelOrderTraversal {
+    public static class Node<T> {
+        public T val;
+        public Node<T> left;
+        public Node<T> right;
+
+        public Node(T val) {
+            this(val, null, null);
+        }
+
+        public Node(T val, Node<T> left, Node<T> right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<List<Integer>> levelOrderTraversal(Node<Integer> root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        ArrayDeque<Node<Integer>> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                Node<Integer> node = queue.pop();
+                level.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+    public static <T> Node<T> buildTree(Iterator<String> iter, Function<String, T> f) {
+        String val = iter.next();
+        if (val.equals("x")) return null;
+        Node<T> left = buildTree(iter, f);
+        Node<T> right = buildTree(iter, f);
+        return new Node<T>(f.apply(val), left, right);
+    }
+
+    public static List<String> splitWords(String s) {
+        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Node<Integer> root = buildTree(splitWords(scanner.nextLine()).iterator(), Integer::parseInt);
+        scanner.close();
+        List<List<Integer>> res = levelOrderTraversal(root);
+        for (List<Integer> row : res) {
+            System.out.println(row.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+        }
+    }
+}
