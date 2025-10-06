acc9d330d9a02fd29522dedb391a47f38bcf8016
owrmille <iasemin.topchu@gmail.com>
2025-10-06 22:59:58 +0200

    Add OpenTheLock

--- algomonster/Graph/OpenTheLock.java
+++ algomonster/Graph/OpenTheLock.java
@@ -1,4 +1,7 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -23,7 +26,36 @@ public class OpenTheLock {
                                                             .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
 
     public static int numSteps(String targetCombo, List<String> trappedCombos) {
-        
+        if (targetCombo.equals("0000")) {
+            return 0;
+        }
+        HashSet<String> trappedComboSet = new HashSet<>(trappedCombos);
+        HashMap<String, Integer> steps = new HashMap<>();
+        ArrayDeque<String> bfsQueue = new ArrayDeque<>();
+        steps.put("0000", 0);
+        bfsQueue.offer("0000");
+        while (!bfsQueue.isEmpty()) {
+            String top = bfsQueue.poll();
+            for (int i = 0; i < 4; i++) {
+                String newCombo = top.substring(0, i).concat(String.valueOf(prevDigit.get(top.charAt(i)))).concat(top.substring(i + 1));
+                if (!trappedComboSet.contains(newCombo) && !steps.containsKey(newCombo)) {
+                    bfsQueue.offer(newCombo);
+                    steps.put(newCombo, steps.get(top) + 1);
+                    if (newCombo.equals(targetCombo)) {
+                        return steps.get(newCombo);
+                    }
+                }
+                newCombo = top.substring(0, i).concat(String.valueOf(nextDigit.get(top.charAt(i)))).concat(top.substring(i + 1));
+                if (!trappedComboSet.contains(newCombo) && !steps.containsKey(newCombo)) {
+                    bfsQueue.offer(newCombo);
+                    steps.put(newCombo, steps.get(top) + 1);
+                    if (newCombo.equals(targetCombo)) {
+                        return steps.get(newCombo);
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     public static List<String> splitWords(String s) {
