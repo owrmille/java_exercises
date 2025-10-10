65732f1c1f33dbaa7b66466d7e850fa164478595
owrmille <iasemin.topchu@gmail.com>
2025-10-11 00:22:01 +0200

    Switched to my version of ReconstructingSequence

--- algomonster/Graph/ReconstructingSequence.java
+++ algomonster/Graph/ReconstructingSequence.java
@@ -5,65 +5,68 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ReconstructingSequence {
+    public static <T> Map<T, Integer> findInDegree(Map<T, Set<T>> graph) {
+        Map<T, Integer> inDegree = new HashMap<>();
+        graph.keySet().forEach(key -> {
+            inDegree.put(key, 0);
+        });
+        graph.entrySet().forEach(entry -> {
+            for (T neighbour : entry.getValue()) {
+                inDegree.put(neighbour, inDegree.get(neighbour) + 1);
+            }
+        });
+        return inDegree;
+    }
+
+    public static <T> boolean topoSort(Map<T, Set<T>> graph, List<T> original) {
+        List<T> res = new ArrayList<>();
+        Queue<T> queue = new ArrayDeque<>();
+        Map<T, Integer> inDegree = findInDegree(graph);
+
+        inDegree.entrySet().forEach(entry -> {
+            if (entry.getValue() == 0) {
+                queue.add(entry.getKey());
+            }
+        });
+
+        while (!queue.isEmpty()) {
+            if (queue.size() > 1) {
+                return false;
+            }
+            T node = queue.poll();
+            res.add(node);
+            for (T neighbour :  graph.get(node)) {
+                inDegree.put(neighbour, inDegree.get(neighbour) - 1);
+                if (inDegree.get(neighbour) == 0) {
+                    queue.add(neighbour);
+                }
+            }
+        }
+        return original.equals(res);
+    }
+
     public static boolean sequenceReconstruction(List<Integer> original, List<List<Integer>> seqs) {
         int n = original.size();
-        Map<Integer, Set<Integer>> graph = new HashMap<>(); // using set here cuz possibility of repeated sequences
+        Map<Integer, Set<Integer>> graph = new HashMap<>();  // set -> no repeptitions of sequences
         for (int i = 1; i <= n; i++) {
             graph.put(i, new HashSet<>());
         }
         for (List<Integer> seq : seqs) {
             for (int i = 0; i < seq.size() - 1; i++) {
-                int earlyNum = seq.get(i), lateNum = seq.get(i + 1);
-                if (!graph.get(earlyNum).contains(lateNum)) {
-                    graph.get(earlyNum).add(lateNum);
+                int earlyVal = seq.get(i);
+                int lateVal = seq.get(i + 1);
+                if (!graph.get(earlyVal).contains(lateVal)) {
+                    graph.get(earlyVal).add(lateVal);
                 }
             }
         }
         return topoSort(graph, original);
-    }
-
-    public static <T> Map<T, Integer> findInDegree(Map<T, Set<T>> graph) {
-        Map<T, Integer> indegree = new HashMap<>();
-        graph.keySet().forEach(node -> {
-            indegree.put(node, 0);
-        });
-        graph.entrySet().forEach(entry -> {
-            for (T neighbor : entry.getValue()) {
-                indegree.put(neighbor, indegree.get(neighbor) + 1);
-            }
-        });
-        return indegree;
-    }
-
-    public static <T> boolean topoSort(Map<T, Set<T>> graph, List<T> original) {
-        List<T> reconstructed = new ArrayList<>();
-        ArrayDeque<T> q = new ArrayDeque<>();
-        Map<T, Integer> indegree = findInDegree(graph);
-        indegree.entrySet().forEach(entry -> {
-            if (entry.getValue() == 0) {
-                q.add(entry.getKey());
-            }
-        });
-        while (!q.isEmpty()) {
-            if (q.size() > 1) { // if there's > 1 node in the queue, the reconstruction is not unique
-                return false;
-            }
-            T node = q.poll();
-            reconstructed.add(node);
-            for (T neighbor : graph.get(node)) {
-                indegree.put(neighbor, indegree.get(neighbor) - 1);
-                if (indegree.get(neighbor) == 0) {
-                    q.add(neighbor);
-                }
-            }
-        }
-        // check if sequence is the same as original sequence
-        return original.equals(reconstructed);
     }
 
     public static List<String> splitWords(String s) {
